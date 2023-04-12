ff7ab21b7a5313f54f3fd43bbe4e2e69e6d2a790
Paulonbass75 <paulmpool@yahoo.com>
2023-04-12 15:03:29 -0400

    set auth

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,7 +2,6 @@ import { useState } from "react";
 import { useNavigate, useOutletContext } from "react-router-dom";
 import Input from "./form/input";
 
-
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -15,18 +14,37 @@ export default function Login() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("email/pass", email, password);
-    // return [email, password];
+    //build request payload
+    let payload = {
+      email: email,
+      password: password,
+    };
+    const requestOptions = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      credentials: "include",
+      body: JSON.stringify(payload),
+    };
 
-    if (email === "admin@test.com") {
-      setJwtToken("admin");
-      setAlertClassName("d-none");
-      setAlertMessage("");
-      navigate("/");
-    } else {
-      setAlertClassName("alert alert-danger");
-      setAlertMessage("Invalid email/password");
-    }
+    fetch(`/authenticate`, requestOptions)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.error) {
+          setAlertClassName("alert-danger");
+          setAlertMessage(data.message);
+        } else {
+          setJwtToken(data.access_token);
+          setAlertClassName("d-none");
+          setAlertMessage("");
+          navigate("/");
+        }
+      })
+      .catch((error) => {
+        setAlertClassName("alert-danger");
+        setAlertMessage(error);
+      });
   };
 
   return (
